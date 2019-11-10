6a5af86e623aabe22d52a9c54818c1e407d94da5
Raphaël <raphael@digimezzo.com>
2019-11-10 21:39:32 +0100

    Added a test for opening of license dialog

--- src/spec/components/about.component.spec.ts
+++ src/spec/components/about.component.spec.ts
@@ -2,14 +2,17 @@ import * as assert from 'assert';
 import * as TypeMoq from "typemoq";
 import { AboutComponent } from '../../app/components/about/about.component';
 import { Constants } from '../../app/core/constants';
+import { MatDialog } from '@angular/material';
+import { Times } from "typemoq";
 
 describe('AboutComponent', () => {
     describe('constructor', () => {
         it('Should provide application version', () => {
             // Arrange
+            var matDialogMock = TypeMoq.Mock.ofType<MatDialog>();
         
             // Act
-            let aboutComponent: AboutComponent = new AboutComponent();
+            let aboutComponent: AboutComponent = new AboutComponent(matDialogMock.object);
 
             // Assert
             assert.equal(aboutComponent.applicationVersion, Constants.applicationVersion);
@@ -17,12 +20,27 @@ describe('AboutComponent', () => {
 
         it('Should provide application copyright', () => {
             // Arrange
-        
+            var matDialogMock = TypeMoq.Mock.ofType<MatDialog>();
+
             // Act
-            let aboutComponent: AboutComponent = new AboutComponent();
+            let aboutComponent: AboutComponent = new AboutComponent(matDialogMock.object);
 
             // Assert
             assert.equal(aboutComponent.applicationCopyright, Constants.applicationCopyright);
+        });
+    });
+
+    describe('openLicenseDialog', () => {
+        it('Should open the license dialog', () => {
+            // Arrange
+            var matDialogMock = TypeMoq.Mock.ofType<MatDialog>();
+            let aboutComponent: AboutComponent = new AboutComponent(matDialogMock.object);
+
+            // Act
+            aboutComponent.openLicenseDialog();
+
+            // Assert
+            matDialogMock.verify(x => x.open(TypeMoq.It.isAny(), TypeMoq.It.isAny()), Times.atLeastOnce());
         });
     });
 });
