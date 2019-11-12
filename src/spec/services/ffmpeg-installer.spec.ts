44d2e7c5ae14e218c1a6c9520fdc84945f6265ed
Raphaël <raphael@digimezzo.com>
2019-11-13 00:11:28 +0100

    Fixed tests

--- src/spec/services/ffmpeg-installer.spec.ts
+++ src/spec/services/ffmpeg-installer.spec.ts
@@ -9,7 +9,7 @@ import * as path from 'path';
 
 describe('FFmpegInstaller', () => {
     describe('ensureFFmpegIsAvailableAsync', () => {
-        it('Should download FFmpeg if FFmpeg folder does not exist', () => {
+        it('Should download FFmpeg if FFmpeg folder does not exist', async () => {
             // Arrange
             var fileSystemMock = TypeMoq.Mock.ofType<FileSystem>();
             var loggerMock = TypeMoq.Mock.ofType<Logger>();
@@ -23,13 +23,13 @@ describe('FFmpegInstaller', () => {
             let ffmpegInstaller: FFmpegInstaller = new FFmpegInstaller(loggerMock.object, ffmpegDownloaderMock.object, fileSystemMock.object);
 
             // Act
-            ffmpegInstaller.ensureFFmpegIsAvailableAsync();
+            await ffmpegInstaller.ensureFFmpegIsAvailableAsync();
 
             // Assert
             ffmpegDownloaderMock.verify(x => x.downloadAsync(ffmpegFolder), Times.exactly(1));
         });
 
-        it('Should download FFmpeg if FFmpeg folder exists but the folder is empty', () => {
+        it('Should download FFmpeg if FFmpeg folder exists but the folder is empty', async () => {
             // Arrange
             var fileSystemMock = TypeMoq.Mock.ofType<FileSystem>();
             var loggerMock = TypeMoq.Mock.ofType<Logger>();
@@ -44,13 +44,13 @@ describe('FFmpegInstaller', () => {
             let ffmpegInstaller: FFmpegInstaller = new FFmpegInstaller(loggerMock.object, ffmpegDownloaderMock.object, fileSystemMock.object);
 
             // Act
-            ffmpegInstaller.ensureFFmpegIsAvailableAsync();
+            await ffmpegInstaller.ensureFFmpegIsAvailableAsync();
 
             // Assert
             ffmpegDownloaderMock.verify(x => x.downloadAsync(ffmpegFolder), Times.exactly(1));
         });
 
-        it('Should download FFmpeg if FFmpeg folder exists and the folder contains a file that does not contain "ffmpeg"', () => {
+        it('Should download FFmpeg if FFmpeg folder exists and the folder contains a file that does not contain "ffmpeg"', async () => {
             // Arrange
             var fileSystemMock = TypeMoq.Mock.ofType<FileSystem>();
             var loggerMock = TypeMoq.Mock.ofType<Logger>();
@@ -65,13 +65,13 @@ describe('FFmpegInstaller', () => {
             let ffmpegInstaller: FFmpegInstaller = new FFmpegInstaller(loggerMock.object, ffmpegDownloaderMock.object, fileSystemMock.object);
 
             // Act
-            ffmpegInstaller.ensureFFmpegIsAvailableAsync();
+            await ffmpegInstaller.ensureFFmpegIsAvailableAsync();
 
             // Assert
             ffmpegDownloaderMock.verify(x => x.downloadAsync(ffmpegFolder), Times.exactly(1));
         });
 
-        it('Should not download FFmpeg if FFmpeg folder exists and the folder contains a file that is "ffmpeg"', () => {
+        it('Should not download FFmpeg if FFmpeg folder exists and the folder contains a file that is "ffmpeg"', async () => {
             // Arrange
             var fileSystemMock = TypeMoq.Mock.ofType<FileSystem>();
             var loggerMock = TypeMoq.Mock.ofType<Logger>();
@@ -86,13 +86,13 @@ describe('FFmpegInstaller', () => {
             let ffmpegInstaller: FFmpegInstaller = new FFmpegInstaller(loggerMock.object, ffmpegDownloaderMock.object, fileSystemMock.object);
 
             // Act
-            ffmpegInstaller.ensureFFmpegIsAvailableAsync();
+            await ffmpegInstaller.ensureFFmpegIsAvailableAsync();
 
             // Assert
             ffmpegDownloaderMock.verify(x => x.downloadAsync(ffmpegFolder), Times.never());
         });
 
-        it('Should not download FFmpeg if FFmpeg folder exists and the folder contains a file that contains "ffmpeg"', () => {
+        it('Should not download FFmpeg if FFmpeg folder exists and the folder contains a file that contains "ffmpeg"', async () => {
             // Arrange
             var fileSystemMock = TypeMoq.Mock.ofType<FileSystem>();
             var loggerMock = TypeMoq.Mock.ofType<Logger>();
@@ -107,10 +107,52 @@ describe('FFmpegInstaller', () => {
             let ffmpegInstaller: FFmpegInstaller = new FFmpegInstaller(loggerMock.object, ffmpegDownloaderMock.object, fileSystemMock.object);
 
             // Act
-            ffmpegInstaller.ensureFFmpegIsAvailableAsync();
+            await ffmpegInstaller.ensureFFmpegIsAvailableAsync();
 
             // Assert
             ffmpegDownloaderMock.verify(x => x.downloadAsync(ffmpegFolder), Times.never());
+        });
+
+        it('Should not set ffmpegPath if ffmpeg is not found', async () => {
+            // Arrange
+            var fileSystemMock = TypeMoq.Mock.ofType<FileSystem>();
+            var loggerMock = TypeMoq.Mock.ofType<Logger>();
+            var ffmpegDownloaderMock = TypeMoq.Mock.ofType<FFmpegDownloader>();
+
+            fileSystemMock.setup(x => x.applicatioDataFolder()).returns(() => "/home/user/.config/Vitomu");
+
+            let ffmpegFolder: string = path.join(fileSystemMock.object.applicatioDataFolder(), "FFmpeg");
+            fileSystemMock.setup(x => x.existsSync(ffmpegFolder)).returns(() => true);
+            fileSystemMock.setup(x => x.readdirSync(ffmpegFolder)).returns(() => []);
+
+            let ffmpegInstaller: FFmpegInstaller = new FFmpegInstaller(loggerMock.object, ffmpegDownloaderMock.object, fileSystemMock.object);
+
+            // Act
+            await ffmpegInstaller.ensureFFmpegIsAvailableAsync();
+
+            // Assert
+            assert.ok(ffmpegInstaller.ffmpegPath.length === 0);
+        });
+
+        it('Should set ffmpegPath if ffmpeg is found', async () => {
+            // Arrange
+            var fileSystemMock = TypeMoq.Mock.ofType<FileSystem>();
+            var loggerMock = TypeMoq.Mock.ofType<Logger>();
+            var ffmpegDownloaderMock = TypeMoq.Mock.ofType<FFmpegDownloader>();
+
+            fileSystemMock.setup(x => x.applicatioDataFolder()).returns(() => "/home/user/.config/Vitomu");
+
+            let ffmpegFolder: string = path.join(fileSystemMock.object.applicatioDataFolder(), "FFmpeg");
+            fileSystemMock.setup(x => x.existsSync(ffmpegFolder)).returns(() => true);
+            fileSystemMock.setup(x => x.readdirSync(ffmpegFolder)).returns(() => ["ffmpeg"]);
+
+            let ffmpegInstaller: FFmpegInstaller = new FFmpegInstaller(loggerMock.object, ffmpegDownloaderMock.object, fileSystemMock.object);
+
+            // Act
+            await ffmpegInstaller.ensureFFmpegIsAvailableAsync();
+
+            // Assert
+            assert.ok(ffmpegInstaller.ffmpegPath.length > 0);
         });
     });
 });
