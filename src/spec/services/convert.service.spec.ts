5d4d9487107c439979bbd9371b0aff99093b622f
Raphaël <raphael@digimezzo.com>
2019-11-14 08:05:41 +0100

    Fixed a test name

--- src/spec/services/convert.service.spec.ts
+++ src/spec/services/convert.service.spec.ts
@@ -2,10 +2,5 @@ import * as assert from 'assert';
 import * as TypeMoq from "typemoq";
 import { Times } from 'typemoq';
 
-describe('CovertService', () => {
-    // describe('constructor', () => {
-    //     it('Should provide a list of themes', () => {
-           
-    //     });
-    // });
+describe('ConvertService', () => {
 });
