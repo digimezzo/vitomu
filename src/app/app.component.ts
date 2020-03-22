dda3396540f08f321b78674c5b3c210d3d4f44c7
Digimezzo <raphael@digimezzo.com>
2020-03-22 05:49:40 +0100

    Font sizes are now custom properties

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AppearanceService } from './services/appearance/appearance.service';
 import { ConvertService } from './services/convert/convert.service';
 import { TranslatorService } from './services/translator/translator.service';
@@ -8,10 +8,17 @@ import { TranslatorService } from './services/translator/translator.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   constructor(private translator: TranslatorService, private appearance: AppearanceService, private convert: ConvertService) {
 
     this.appearance.applyTheme();
     this.translator.applyLanguage();
+  }
+
+  public ngOnInit(): void {
+    // const element = document.documentElement;
+    // element.style.setProperty('--fontsize-normal', '13px');
+    // element.style.setProperty('--fontsize-larger', '18px');
+    // element.style.setProperty('--fontsize-largest', '26px');
   }
 }
