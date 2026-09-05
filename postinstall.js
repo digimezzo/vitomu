315197cfb5163ab9dfeb6a94fb42eab1dd97b245
Digimezzo <digimezzo@outlook.com>
2026-09-05 21:48:14 +0200

    Upgrade to Angular 11

--- postinstall.js
+++ postinstall.js
@@ -1,6 +1,6 @@
 // Allow angular using electron module (native node modules)
 const fs = require('fs');
-const f_angular = 'node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/webpack-configs/browser.js';
+const f_angular = 'node_modules/@angular-devkit/build-angular/src/webpack/configs/browser.js';
 
 fs.readFile(f_angular, 'utf8', function (err, data) {
   if (err) {
