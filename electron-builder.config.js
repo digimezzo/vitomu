16d28fc2b0dc2b4f2109834cd597da35648100a2
Digimezzo <digimezzo@outlook.com>
2026-09-06 11:38:26 +0200

    Fix build

--- electron-builder.config.js
+++ electron-builder.config.js
@@ -58,6 +58,7 @@ const config = {
     linux: {
         target: ['AppImage', 'deb', 'rpm', 'pacman', 'snap'],
         category: 'Audio',
+        syncDesktopName: true,
         artifactName: `\${productName}-${getFullVersion()}.\${ext}`,
         synopsis: 'Easy to use video to audio converter.',
         description:
